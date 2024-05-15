9c7c0b8752f7f08c2a9ca5c4c4bb4e3c8fed0b0f
samir <shubhod2k18@gmail.com>
2024-05-15 18:25:21 +0530

    additional controllers & routes added

--- src/controllers/items.controller.ts
+++ src/controllers/items.controller.ts
@@ -1,7 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 import { ApiError, ApiResponse } from "../utils/responses/responses.ts";
 import { db } from "../utils/db.ts";
-import { Item } from "@prisma/client";
+import { Item, MediaType } from "@prisma/client";
 import crypto from "crypto";
 
 export async function createFile(
@@ -30,6 +30,10 @@ export async function createFile(
 					id: folderId,
 					ownerId: req.userId,
 					isFolder: true,
+					isTrash: false,
+				},
+				select: {
+					id: true,
 				},
 			});
 
@@ -39,7 +43,7 @@ export async function createFile(
 
 			const previewUrl = crypto.randomBytes(12).toString("hex");
 
-			const file = await db.item.create({
+			await db.item.create({
 				data: {
 					name,
 					media,
@@ -96,6 +100,7 @@ export async function getItems(
 					id: folderId,
 					ownerId: req.userId,
 					isFolder: true,
+					isTrash: false,
 				},
 			});
 
@@ -104,16 +109,28 @@ export async function getItems(
 			}
 
 			const items = await db.item.findMany({
-				where: { ownerId: req.userId, parentId: existingFolder.id },
-				include: { childrens: true },
+				where: {
+					ownerId: req.userId,
+					parentId: existingFolder.id,
+					isTrash: false,
+				},
+				include: {
+					_count: {
+						select: { childrens: true },
+					},
+				},
 			});
 
 			return ApiResponse(res, 200, { items });
 		}
 
 		const items = await db.item.findMany({
-			where: { ownerId: req.userId, parent: null },
-			include: { childrens: true },
+			where: { ownerId: req.userId, parent: null, isTrash: false },
+			include: {
+				_count: {
+					select: { childrens: true },
+				},
+			},
 		});
 
 		ApiResponse(res, 200, { items });
@@ -148,6 +165,10 @@ export async function createFolder(
 					id: folderId,
 					ownerId: req.userId,
 					isFolder: true,
+					isTrash: false,
+				},
+				select: {
+					id: true,
 				},
 			});
 
@@ -189,6 +210,175 @@ export async function createFolder(
 		});
 
 		ApiResponse(res, 201, { message: "Folder created" });
+	} catch (error) {
+		next(error);
+	}
+}
+
+export async function getItemsByType(
+	req: Request,
+	res: Response,
+	next: NextFunction
+) {
+	try {
+		if (!req.userId) {
+			throw new ApiError(401, "Unauthorized");
+		}
+
+		const { type } = req.query;
+
+		if (!type || typeof type !== "string") {
+			throw new ApiError(400, "Required field is missing");
+		}
+
+		const items = await db.item.findMany({
+			where: {
+				mediaType:
+					type === "PDF"
+						? MediaType.PDF
+						: type === "IMAGE"
+						? MediaType.IMAGE
+						: type === "VIDEO"
+						? MediaType.VIDEO
+						: null,
+				ownerId: req.userId,
+				isTrash: false,
+			},
+		});
+
+		ApiResponse(res, 200, { items });
+	} catch (error) {
+		next(error);
+	}
+}
+
+export async function shareItem(
+	req: Request,
+	res: Response,
+	next: NextFunction
+) {
+	try {
+		if (!req.userId) {
+			throw new ApiError(401, "Unauthorized");
+		}
+
+		const { userIds }: { userIds: string[] } = req.body;
+
+		if (!userIds || userIds.length === 0) {
+			throw new ApiError(400, "Required field is missing");
+		}
+
+		const itemId = req.params.itemId;
+
+		if (!itemId) {
+			throw new ApiError(400, "Required query is missing");
+		}
+
+		const item = await db.item.findUnique({
+			where: { id: itemId, isTrash: false, ownerId: req.userId },
+			select: {
+				id: true,
+				sharedWithUsers: true,
+			},
+		});
+
+		if (!item) {
+			throw new ApiError(404, "Item not found");
+		}
+
+		await db.item.update({
+			where: { id: item.id },
+			data: {
+				sharedWithUsers: { set: [...item.sharedWithUsers, ...userIds] },
+			},
+		});
+
+		ApiResponse(res, 200, { message: "Item is shared" });
+	} catch (error) {
+		next(error);
+	}
+}
+
+export async function makeTrash(
+	req: Request,
+	res: Response,
+	next: NextFunction
+) {
+	try {
+		if (!req.userId) {
+			throw new ApiError(401, "Unauthorized");
+		}
+
+		const itemId = req.params.itemId;
+
+		if (!itemId) {
+			throw new ApiError(400, "Required query is missing");
+		}
+
+		const item = await db.item.findUnique({
+			where: { id: itemId, isTrash: false, ownerId: req.userId },
+			select: {
+				id: true,
+			},
+		});
+
+		if (!item) {
+			throw new ApiError(404, "Item not found");
+		}
+
+		await db.item.update({
+			where: { id: item.id },
+			data: {
+				isTrash: true,
+			},
+		});
+
+		await db.item.updateMany({
+			where: { parentId: item.id, isTrash: false },
+			data: {
+				isTrash: true,
+			},
+		});
+
+		ApiResponse(res, 200, { message: "File sent to trash" });
+	} catch (error) {
+		next(error);
+	}
+}
+
+export async function deleteItem(
+	req: Request,
+	res: Response,
+	next: NextFunction
+) {
+	try {
+		if (!req.userId) {
+			throw new ApiError(401, "Unauthorized");
+		}
+
+		const itemId = req.params.itemId;
+
+		if (!itemId) {
+			throw new ApiError(400, "Required query is missing");
+		}
+
+		const item = await db.item.findUnique({
+			where: { id: itemId, isTrash: true, ownerId: req.userId },
+		});
+
+		if (!item) {
+			throw new ApiError(404, "Item not found");
+		}
+
+		await db.item.deleteMany({
+			where: { parentId: item.id, isTrash: true },
+		});
+
+		await db.item.delete({
+			where: { id: item.id },
+		});
+
+		ApiResponse(res, 200, { message: "File is deleted" });
 	} catch (error) {
 		next(error);
 	}
--- src/routes/items.route.ts
+++ src/routes/items.route.ts
@@ -1,11 +1,25 @@
 import { Router } from "express";
 import { verifyJWT } from "../middleware/verify-jwt.ts";
-import { createFile, createFolder, getItems } from "../controllers/items.controller.ts";
+import {
+	createFile,
+	createFolder,
+	deleteItem,
+	getItems,
+	getItemsByType,
+	makeTrash,
+	shareItem,
+} from "../controllers/items.controller.ts";
 
 const router = Router();
 
-router.get("/items/*", verifyJWT, getItems);
-router.post("/files/*", verifyJWT, createFile);
-router.post("/folders/*", verifyJWT, createFolder);
+router.use(verifyJWT);
+
+router.get("/items/*", getItems);
+router.get("/files", getItemsByType);
+router.post("/files/*", createFile);
+router.post("/folders/*", createFolder);
+router.put("/items/:itemId", shareItem);
+router.patch("/items/:itemId", makeTrash);
+router.delete("/items/:itemId", deleteItem);
 
 export default router;
