3fd72f69b4c758daaef0bdbfaf9c739c5c9d3715
samir <shubhod2k18@gmail.com>
2024-07-09 20:15:30 +0530

    fix: path error

--- dist/utils/multer-upload.js
+++ dist/utils/multer-upload.js
@@ -5,14 +5,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.storage = void 0;
 const multer_1 = __importDefault(require("multer"));
-const path_1 = __importDefault(require("path"));
-const url_1 = require("url");
-const __filename = (0, url_1.fileURLToPath)(require.main?.path);
-const __dirname = path_1.default.dirname(__filename);
 exports.storage = multer_1.default.diskStorage({
-    destination(_, __, callback) {
-        callback(null, path_1.default.join(__dirname, "./public/temp"));
-    },
+    destination: require.main?.path + "/" + "public/images/",
     filename(_, file, callback) {
         callback(null, crypto.randomUUID().slice(0, 5) +
             "-" +
--- src/utils/multer-upload.ts
+++ src/utils/multer-upload.ts
@@ -1,14 +1,7 @@
 import multer from "multer";
-import path from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(require.main?.path!);
-const __dirname = path.dirname(__filename);
 
 export const storage = multer.diskStorage({
-	destination(_, __, callback) {
-		callback(null, path.join(__dirname, "./public/temp"));
-	},
+	destination: require.main?.path + "/" + "public/images/",
 	filename(_, file, callback) {
 		callback(
 			null,
